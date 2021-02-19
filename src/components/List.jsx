3b5e857de632fdc9c6a83ece7707bc87a090c224
Zumma123 <78385658+Zumma123@users.noreply.github.com>
2021-02-19 11:54:23 +0200

    Update List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,14 +5,6 @@ export const List = (props) => {
   const users = props.users.filter((u) =>
   u.toLowerCase().includes(props.search.toLowerCase()) 
   );
-  if(props.search !== users){
-      console.log('None')
-  } else{
-      console.log("yes")
-  }
-
-//   console.log(props.users)
-
   return (
     <ul className="list-group">
       {users.map((u, idx) => (
